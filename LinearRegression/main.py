13947399752a294d24f2f38d666a0ddc6337c3f3
matheves <samuel.mathevet@st.com>
2022-01-27 11:17:47 +0100

    fix: Linear regression with cross validation

--- LinearRegression/main.py
+++ LinearRegression/main.py
@@ -1,20 +1,41 @@
 import numpy as np
 import pandas as pd
 import sklearn as sk
-import keras
+import re
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from collections import Counter
-from keras.utils import to_categorical
+from sklearn.model_selection import cross_val_score
+
+def getColEncoded(name, dataset):
+    return [x for x in dataset.columns if re.search(name + " \d+", str(x))]
+
+# Get dataset column
+amenitiesList = ["Internet", "Air conditioning", "Breakfast", "TV", "Bathub", "Dryer", "Elevator in building", "Parking",
+                     "Gym", "Heating", "Kitchen", "Pets allowed", "Pool", "Smoking allowed", "Washer", "Wheelchair accessible"]
 
 # Get and modify dataset
 dataset = pd.read_csv("../Dataset/datasetFilter.csv")
-dataset = dataset[dataset['Price'].notna()]
-data = dataset[["Square Meter", "AmenitiesScore","Latitude", "Longitude"]].values
+
+neiColEncodedNeighbourhood = getColEncoded("Neighbourhood", dataset)
+neiColEncodedPropertyType = getColEncoded("Property Type", dataset)
+neiColEncodedRoomType = getColEncoded("Room Type", dataset)
+neiColEncodedBedType = getColEncoded("Bed Type", dataset)
+
+data = dataset[["Square Meter"] +
+                ["Accommodates"] +
+                ["Bathrooms"] +
+                ["Bedrooms"] +
+                ["Guests Included"] +
+                amenitiesList +
+                neiColEncodedNeighbourhood +
+                neiColEncodedPropertyType +
+                neiColEncodedRoomType +
+                neiColEncodedBedType].values
+
 target = dataset['Price']
 
 # Create features and target for training and testing
-x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.33)
+x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2)
 
 # Create model & train it
 reg = LinearRegression().fit(x_train, y_train)
@@ -24,13 +45,12 @@ test_score = reg.score(x_test,y_test)
 
 print("Training set score: {:.2f} ".format(train_score))
 print("Test set score: {:.2f} ".format(test_score))
-print(Counter(dataset['Property Type']).keys())
 
-encoded = to_categorical(dataset['Property Type'])
-print(encoded)
-# invert encoding
-inverted = argmax("Boat")
-print(inverted)
+# Cross validation
+scores = cross_val_score(reg, data, target, cv=10)
+
+print("Cross validation mean score : {:.2f}".format(scores.mean()))
+
 
 
 
