2c8aa3d49b033a0566838b18cd92e14b122b2e76
matheves <samuel.mathevet@st.com>
2022-01-27 11:43:39 +0100

    feat: Add utils module to get dataset

--- LinearRegression/main.py
+++ LinearRegression/main.py
@@ -1,41 +1,19 @@
 import numpy as np
-import pandas as pd
 import sklearn as sk
-import re
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
+import sys
+sys.path.insert(0, '..')
+from utils import GetDataSet
 
 def getColEncoded(name, dataset):
     return [x for x in dataset.columns if re.search(name + " \d+", str(x))]
 
-# Get dataset column
-amenitiesList = ["Internet", "Air conditioning", "Breakfast", "TV", "Bathub", "Dryer", "Elevator in building", "Parking",
-                     "Gym", "Heating", "Kitchen", "Pets allowed", "Pool", "Smoking allowed", "Washer", "Wheelchair accessible"]
-
-# Get and modify dataset
-dataset = pd.read_csv("../Dataset/datasetFilter.csv")
-
-neiColEncodedNeighbourhood = getColEncoded("Neighbourhood", dataset)
-neiColEncodedPropertyType = getColEncoded("Property Type", dataset)
-neiColEncodedRoomType = getColEncoded("Room Type", dataset)
-neiColEncodedBedType = getColEncoded("Bed Type", dataset)
-
-data = dataset[["Square Meter"] +
-                ["Accommodates"] +
-                ["Bathrooms"] +
-                ["Bedrooms"] +
-                ["Guests Included"] +
-                amenitiesList +
-                neiColEncodedNeighbourhood +
-                neiColEncodedPropertyType +
-                neiColEncodedRoomType +
-                neiColEncodedBedType].values
-
-target = dataset['Price']
+X, Y = GetDataSet("../Dataset/datasetFilter.csv")
 
 # Create features and target for training and testing
-x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 # Create model & train it
 reg = LinearRegression().fit(x_train, y_train)
@@ -47,7 +25,7 @@ print("Training set score: {:.2f} ".format(train_score))
 print("Test set score: {:.2f} ".format(test_score))
 
 # Cross validation
-scores = cross_val_score(reg, data, target, cv=10)
+scores = cross_val_score(reg, X, Y, cv=10)
 
 print("Cross validation mean score : {:.2f}".format(scores.mean()))
 
---
+++ utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:27:20 2022
+
+@author: samue
+"""
+import pandas as pd
+import re
+
+amenitiesList = ["Internet", "Air conditioning", "Breakfast", "TV", "Bathub", "Dryer", "Elevator in building", "Parking",
+                     "Gym", "Heating", "Kitchen", "Pets allowed", "Pool", "Smoking allowed", "Washer", "Wheelchair accessible"]
+
+def getColEncoded(name, dataset):
+    return [x for x in dataset.columns if re.search(name + " \d+", str(x))]
+
+def GetDataSet(path):
+    dataset = pd.read_csv(path, dtype={'Zipcode': "string"})
+    
+    neiColEncodedNeighbourhood = getColEncoded("Neighbourhood", dataset)
+    neiColEncodedPropertyType = getColEncoded("Property Type", dataset)
+    neiColEncodedRoomType = getColEncoded("Room Type", dataset)
+    neiColEncodedBedType = getColEncoded("Bed Type", dataset)
+    
+    X = dataset[["Square Meter"] +
+                    ["Accommodates"] +
+                    ["Bathrooms"] +
+                    ["Bedrooms"] +
+                    ["Guests Included"] +
+                    amenitiesList +
+                    neiColEncodedNeighbourhood +
+                    neiColEncodedPropertyType +
+                    neiColEncodedRoomType +
+                    neiColEncodedBedType].values
+    
+    Y = dataset['Price']
+    
+    return X,Y
+    
